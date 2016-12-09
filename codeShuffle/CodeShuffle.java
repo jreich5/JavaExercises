c36150a6665ea657811d608c197bddc29f536ca5
Justin Reich <justinreich.dev@gmail.com>
2016-12-09 17:01:42 -0600

    Shuffled Java code to rearrange.

---
+++ codeShuffle/CodeShuffle.java
@@ -0,0 +1,38 @@
+public class CodeShuffle {
+
+    public static void main(String [] args) {
+    
+
+
+    }        
+
+}
+
+
+if (x == 1) {
+
+    System.out.print("d");
+    x = x - 1
+
+}
+
+if (x == 2) {
+    
+    System.out.print("b c ");
+
+}
+
+if (x > 2) {
+
+    System.out.print("a");
+
+}
+
+int x = 3;
+
+x = x - 1;
+
+while (x > 0) {
+
+
+
