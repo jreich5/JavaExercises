2719f2b86939339519f7bb035d2004af1315ab20
Justin Reich <justinreich.dev@gmail.com>
2016-12-09 12:18:34 -0600

    First Java program

---
+++ helloJava/HelloJava.java
@@ -0,0 +1,11 @@
+public class HelloJava {
+    public static void main (String[] args) {
+	int x = 2;
+	int y = 10;
+	while (x < y) {
+	    int result = x;
+	    System.out.print(result);
+	    x += 1;
+	}
+    }
+}
