da9f6ee7fcd4774498ea0b04481ea8cb284eca83
Justin Reich <justinreich.dev@gmail.com>
2016-12-10 16:12:51 -0600

    Compiler exercise

---
+++ beTheComplier/Compiler.java
@@ -0,0 +1,64 @@
+/*
+
+class Exercise1b {
+
+    public static void main(String[] args) {
+
+        int x = 1;
+	
+        while (x < 10) {
+            if (x > 3) {
+                System.out.println("big x");              
+            }
+        }     
+    }
+}
+
+*/
+
+
+
+
+
+// exercise B is not inside a class and will not compile
+
+/*
+
+public static void main(String [] args) {
+
+    int x = 5;
+    while (x > 1) {
+        x = x -1;
+        if (x < 3) {
+            System.out.println("small x");
+
+        }
+    }
+
+}
+
+*/
+
+// Exercise C will not compile because there is no main method declared
+
+/*
+
+class Exercise1B {
+
+    int x = 5;
+    while (x > 1) {
+    
+        x = x - 1;
+        
+        if (x < 3) {
+        
+            System.out.println("small x");
+       
+        }    
+
+    }
+    
+
+}
+
+*/
