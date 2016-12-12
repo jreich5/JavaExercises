3858534c63b87a54402cc5fba885cf380ae2a0d8
Justin Reich <justinreich.dev@gmail.com>
2016-12-11 18:09:32 -0600

    Guess the number pseudo code

---
+++ guessTheNumber/GuessGame.java
@@ -0,0 +1,16 @@
+public class GuessGame() {
+    // startGame method
+        // generate three players
+        // generate the computer number
+        // runRound
+    // generate player method
+    // generate computer number
+    // runRound method
+        // output computer number
+        // output player guesses
+        // output round outcome
+        // if no player won game, runRound again
+    
+    // endGame method
+         // declare the winner(s)!
+}
